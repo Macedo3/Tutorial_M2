25816738030ad6813404df3583a640654f049ba3
Gabriel de Macedo Santos <gabriel.macedo@sou.inteli.edu.br>
2023-04-19 20:29:04 -0300

    add: comentários no codigo

--- SEMANA_01/01_TUTORIAL/app_11.js
+++ SEMANA_01/01_TUTORIAL/app_11.js
@@ -1,8 +1,14 @@
+// Pede a requisição do protocolo http pra rodar
 const http = require('http');
+// Setando o ip 
 const hostname = '127.0.0.1';
+// Setando a porta 
 const port = 3011;
+// Criando o servidor com a requisição e a porta
 const server = http.createServer((req, res) => {
+  // Coloca o status do servidor como 200
   res.statusCode = 200;
+  // Abre o servidor HTML (Front-end)
   res.setHeader('Content-Type', 'text/html');
   res.end('<!DOCTYPE html> <head> <title>Etapa 0 - INSTALACAO</title></head> \
               <body>\
@@ -12,6 +18,8 @@ const server = http.createServer((req, res) => {
               </body> \
            </html>');
 });
+// Le o ip e a porta do servidor
 server.listen(port, hostname, () => {
+  // Mostra toda a porta no console
   console.log(`Server running at http://${hostname}:${port}/`);
 });
--- SEMANA_01/01_TUTORIAL/app_12.js
+++ SEMANA_01/01_TUTORIAL/app_12.js
@@ -1,21 +1,31 @@
+// Faz as mesmas coisas que o ínicio do bloco em app_11.js, porém define uma porta diferente
 const http    = require('http');
 const sqlite3 = require('sqlite3').verbose(); 
 const hostname = '127.0.0.1';
 const port = 3012;
+//
 
+// Faz as mesmas coisas que no bloco const server em app_11.js
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
+  // 
+
+  // Define a variável db como um 'fork' da base de dados "dbUser.db"
   var db = new sqlite3.Database('dbUser.db');
+  // Está coletando os dados
   db.get('SELECT * \
           FROM tbUser \
           WHERE userId = 1', [], (err, row) => {
+    // Caso a coleta foi bem feita de maneira correta, é aberto uma pagina em HTML com as etapas escritas feitas.
 		res.write("<h1> Etapa 1 - INSTALACAO </h1>") 
     res.write("<h2> Servidor de Banco de Dados SQLite3 </h2>") 
 	  res.write("<h3> " + row.title + "</h3>"); 
 	  res.end(); 
    });
 });
+// O servidor le a porta e o hostname para enviar pro console esses dados
 server.listen(port, hostname, () => {
+  // Console printa esses dados concatenados
   console.log(`Server running at http://${hostname}:${port}/`);
 });
